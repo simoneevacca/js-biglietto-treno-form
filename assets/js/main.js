63b5dd40ab3c43048d2f1e1ab852975a94fcc256
simoneevacca <v.simone.vacca@gmail.com>
2024-02-20 15:38:36 +0100

    fix input type and add conditions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
-const distanceElement = document.getElementById('distance')
-console.log(distanceElement.value);
 
+const distanceElement = document.getElementById('distance')
 const ageElement = document.getElementById('age')
-console.log(ageElement.value);
+
+const minorsDiscount = 0.2;
+const over65Discount = 0.4;
+console.log("LOG TEST " + distanceElement.value);
+console.log("LOG TEST " + ageElement.value);
+
+
 
 document.querySelector('button').addEventListener('click',
 function(){
+    let ticketPrice = distanceElement.value * 0.21;
+    
     console.log(distanceElement.value * 0.21);
+    console.log(ageElement.value)
+    
+    if (ageElement.value < 18) {
+        
+        ticketPrice = ticketPrice - ticketPrice * minorsDiscount
+        
+    } else if (ageElement.value > 64) {
+        
+        ticketPrice = ticketPrice - ticketPrice * over65Discount
+        
+    }
+    
+    console.log("ticket price is: " + ticketPrice);
 })
