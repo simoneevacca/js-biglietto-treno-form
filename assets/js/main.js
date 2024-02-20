27d0db5a448276bb1853d29af0b59dfdefb5ccf3
simoneevacca <v.simone.vacca@gmail.com>
2024-02-20 16:32:34 +0100

    add name input and print him on ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,22 @@
 
 const distanceElement = document.getElementById('distance')
 const ageElement = document.getElementById('age')
-
+const passengerName = document.getElementById('name')
 const minorsDiscount = 0.2;
 const over65Discount = 0.4;
 console.log("LOG TEST " + distanceElement.value);
 console.log("LOG TEST " + ageElement.value);
+console.log("LOG TEST " + passengerName.value);
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -15,7 +26,14 @@ function(){
     
     console.log(distanceElement.value * 0.21);
     console.log(ageElement.value)
+    console.log(passengerName.value);
+
     
+    document.getElementById('ticketname').innerHTML = passengerName.value
+
+
+
+
     if (ageElement.value < 18) {
         
         ticketPrice = ticketPrice - ticketPrice * minorsDiscount
